b1903f8a93d144a31c96d181b14794a40f51aff2
UstyUsty <alexsysalov07@gmail.com>
2025-12-09 12:27:13 +0700

    fd

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
   
   return (
     <div>
-      <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-950">
-      <div className="w-full max-w-sm space-y-6 border border-white p-5 rounded-xl">
-        <div className="flex items-center flex-row gap-2">
-          <div className="w-full border border-white text-end px-1.5 pt-0.5 rounded-md overflow-hidden">
+      <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-linear-120 from-slate-900 to-black">
+      <div className="w-full max-w-sm space-y-6 p-5 rounded-xl bg-gray-900">
+        <div className="flex items-center flex-row gap-3">
+          <div className="w-full text-end px-1.5 pt-0.5 rounded-md overflow-hidden bg-gray-800">
             <Display text={text} setCount={(text) => setCount(text)}/>
           </div>
           <div className="gap-2 py-5">
             <SendBtn func={() => {send()}}/>
           </div>
         </div>
-        <div className="container justify-center  gap-4 rounded-lg w-auto animate-fadeIn">
+        <div className="container justify-center gap-4 rounded-lg w-auto animate-fadeIn">
           {<Numpad n={text} add={(text) => {plus(text)}}/>}
         </div>
       </div>
@@ -37,16 +37,15 @@ function SendBtn({func}:{func:(param: void) => void}){
   return(
     <div>
       <button className="
-      group
-        border rounded-xl
-        hover:bg-black
-        hover:border-white
+        rounded-lg
+        bg-gray-800
+        hover:bg-blue-200/15
         active:bg-gray-100/15
         active:scale-90
         transition-colors
         duration-100"
       onClick={() => {func()}}
-      ><div className="flex border min-w-15 flex-col items-center px-1.5 py-0.5 rounded-md ">{"->"}</div>
+      ><div className="flex min-w-15 flex-col items-center px-1.5 py-0.5 rounded-md ">{"->"}</div>
       </button>
     </div>
   )
@@ -55,7 +54,7 @@ function SendBtn({func}:{func:(param: void) => void}){
 
 function Numpad({add}:{n: string, add:(param: string)=> void}){
   return(
-    <div className="grid grid-cols-5 gap-2">
+    <div className="grid grid-cols-5 gap-3">
       <Butn text={1} func={() => {add("1")}}/>
       <Butn text={2} func={() => {add("2")}}/>
       <Butn text={3} func={() => {add("3")}}/>
@@ -75,8 +74,9 @@ function Butn({text, func}:{text: number, func:(param: number) => void}){
   return(  
   <div>
       <button className=" h-15 w-15 
-        border rounded-xl
-        hover:bg-black
+                rounded-lg
+        bg-gray-800
+        hover:bg-blue-200/15
         hover:border-white
         active:bg-gray-100/15
         active:scale-90
